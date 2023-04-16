994af8ddb23421f155a6697437578189387c0fb7
Sona <51908834+Kojima-Tatsuki@users.noreply.github.com>
2023-04-16 19:41:38 +0900

    Unityへ対応すべく、file移動とglobal usingの削除

---
+++ src/Domain/Currency/Currency.cs
@@ -0,0 +1,143 @@
+﻿using System;
+
+namespace BlackSmith.Domain.Currency
+{
+    /// <summary> お金 </summary>
+    public class Currency
+    {
+        public CurrencyType Type { get; }
+
+        public int Value => value.Value;
+        private readonly CurrencyValue value;
+
+        /// <summary>お金を生成する</summary>
+        /// <remarks>通貨とその量を元に生成する</remarks>
+        /// <param name="type">通貨</param>
+        /// <param name="value">量</param>
+        public Currency(CurrencyType type, int value)
+        {
+            Type = type;
+            this.value = new CurrencyValue(value);
+        }
+
+        public Currency Add(Currency other)
+        {
+            if (!Type.Equals(other.Type))
+                throw new ArgumentException($"通貨単位が一致していません [this: {Type}], [other: {other.Type}]");
+
+            return new Currency(Type, value.Add(new CurrencyValue(other.Value)).Value);
+        }
+
+        public Currency Subtract(Currency other)
+        {
+            if (!Type.Equals(other.Type))
+                throw new ArgumentException($"通貨単位が一致していません [this: {Type}], [other: {other.Type}]");
+
+            var otherValue = new CurrencyValue(other.Value);
+
+            return new Currency(Type, value.Subtract(otherValue).Value);
+        }
+
+        /// <summary>外貨両替を行う</summary>
+        /// <remarks>端数は切り捨てられる</remarks>
+        /// <param name="type">両替先の型</param>
+        /// <param name="money">変換するお金</param>
+        /// <returns></returns>
+        public Currency Exchange(CurrencyType type)
+        { 
+            var value = (int)Math.Floor((float)type / (float)Type * Value);
+
+            return new(type, value);
+        }
+
+        /// <summary>
+        /// 通貨型が一致するかを返す
+        /// </summary>
+        /// <param name="other">対象の通貨</param>
+        /// <returns>一致していれば真を返す</returns>
+        public bool EqualsType(Currency other) => Type.Equals(other.Type);
+
+        public bool Equals(Currency? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return Value.Equals(other.Value) && EqualsType(other);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+
+            if (GetType() != obj.GetType()) return false;
+            return Equals((Currency)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string? ToString()
+        {
+            return $"Type : {Type}, Value : {Value}";
+        }
+
+        public static bool operator ==(Currency x, Currency y) => x.Equals(y);
+
+        public static bool operator !=(Currency x, Currency y) => !x.Equals(y);
+
+        private class CurrencyValue
+        {
+            public int Value { get; }
+
+            public CurrencyValue(int value)
+            {
+                if (!IsValidValue(value))
+                    throw new AggregateException($"通貨の値として不正な値が入力されました: value = {value}");
+
+                Value = value;
+            }
+
+            public CurrencyValue Add(CurrencyValue other)
+            {
+                return new CurrencyValue(Value + other.Value);
+            }
+
+            public CurrencyValue Subtract(CurrencyValue other)
+            {
+                var v = Value - other.Value;
+                if (!IsValidValue(v))
+                    v = 0;
+
+                return new CurrencyValue(v);
+            }
+
+            /// <summary>
+            /// 正当値かを調べる
+            /// </summary>
+            public static bool IsValidValue(int value)
+            {
+                if (value < 0) return false;
+                return true;
+            }
+
+            public override string ToString()
+            {
+                return Value.ToString();
+            }
+        }
+    }
+
+    // これは、かなり問題を孕んだ実装方式。問題である。
+    // 通貨の価値バランスを保持するドメインサービスを作成し、それに変換を依頼する構造にすべき
+    /// <summary>
+    /// 内部数が倍率を表す
+    /// </summary>
+    public enum CurrencyType
+    {
+        Sakura  = 1, // 桜式通貨
+        Aren    = 2, // アレン式通貨
+    }
+}
---
+++ src/Usecase/Interface/ITranslateInputToActionUsecase.cs
@@ -0,0 +1,12 @@
+﻿using BlackSmith.Domain.Input;
+using System.Collections.Generic;
+
+namespace BlackSmith.Usecase.Interface
+{
+    public interface ITranslateInputToActionUsecase
+    {
+        ActionCode? ExtractionActionCode(IReadOnlyCollection<InputCode> code);
+
+        MoveActionCode? ExtractionMoveActionCode(IReadOnlyCollection<InputCode> codes);
+    }
+}
