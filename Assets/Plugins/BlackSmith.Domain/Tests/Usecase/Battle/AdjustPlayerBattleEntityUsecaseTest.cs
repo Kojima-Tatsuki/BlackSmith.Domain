49de1c1fc799b36390e1ef09124f303524f94f42
Kojima-Tatsuki <51908834+Kojima-Tatsuki@users.noreply.github.com>
2024-12-15 16:56:17 +0900

    BattleCharacterEntityを新規構築するユースケースを作成する (#131)
    * AdjustPlayerBattleEntityUsecaseを作成
    
    * AdjustPlayerBattleEntityUsecaseのテストを追加
    
    * update package json

---
+++ Assets/Plugins/BlackSmith.Domain/Tests/Usecase/Battle/AdjustPlayerBattleEntityUsecaseTest.cs
@@ -0,0 +1,164 @@
+﻿using BlackSmith.Usecase.Interface;
+using Cysharp.Threading.Tasks;
+using System;
+using NUnit.Framework;
+using System.Collections;
+using BlackSmith.Domain.Character.Player;
+using BlackSmith.Domain.Character;
+using System.Threading.Tasks;
+using BlackSmith.Domain.Character.Battle;
+
+#nullable enable
+
+namespace BlackSmith.Usecase.Character.Battle
+{
+    internal class AdjustPlayerBattleEntityUsecaseTest
+    {
+        [Test(Description = "CreateCharacterのテスト")]
+        public async Task CreateCharacterPasses()
+        {
+            var repository = new MockPlayerBattleEntityRepository();
+            var commonEntityCommand = new PlayerCommonReconstructCommand(new CharacterID(), new PlayerName("TestCommonEntity"), new PlayerLevel(Experience.RequiredCumulativeExp(1)));
+            var commonEntity = PlayerFactory.Reconstruct(commonEntityCommand);
+
+            var usecase = new AdjustPlayerBattleEntityUsecase(repository);
+
+            var character = await usecase.CreateCharacter(commonEntity);
+
+            var entity = await repository.FindByID(character.ID);
+
+            if (entity is null)
+                throw new NullReferenceException("RepositoryにBattleCharacterEntityが存在しません");
+
+            Assert.That(character.Equals(entity));
+        }
+
+        private static IEnumerable ReconstructCharacterTestCases()
+        {
+            var repository = new MockPlayerBattleEntityRepository();
+            var commonEntityCommand = new PlayerCommonReconstructCommand(new CharacterID(), new PlayerName("TestCommonEntity"), new PlayerLevel(Experience.RequiredCumulativeExp(1)));
+            var commonEntity = PlayerFactory.Reconstruct(commonEntityCommand);
+            var command = new PlayerBattleReconstructCommand(commonEntity.ID, new CharacterBattleModule(new HealthPoint(commonEntity.Level), new LevelDependentParameters(commonEntity.Level, new Strength(1), new Agility(1)), new BattleEquipmentModule(null, null), new BattleStatusEffectModule(null)));
+
+            yield return new TestCaseData(repository, command, null).SetCategory("正常系");
+
+            // 既に同じIDのプレイヤーが存在する場合
+            yield return new TestCaseData(repository, command, typeof(InvalidOperationException)).SetCategory("異常系");
+        }
+
+        [Test(Description = "ReconstructCharacterのテスト")]
+        [TestCaseSource(nameof(ReconstructCharacterTestCases))]
+        public async Task ReconstructCharacterPasses(IPlayerBattleEntityRepository repository, PlayerBattleReconstructCommand command, Type? exception)
+        {
+            var usecase = new AdjustPlayerBattleEntityUsecase(repository);
+
+            if (exception is null)
+            {
+                var character = await usecase.ReconstructPlayer(command);
+
+                var entity = await repository.FindByID(character.ID);
+
+                if (entity is null)
+                    throw new NullReferenceException("RepositoryにBattleCharacterEntityが存在しません");
+
+                Assert.That(character.Equals(entity));
+            }
+            else
+            {
+                try
+                {
+                    await usecase.ReconstructPlayer(command);
+                }
+                catch (Exception e)
+                {
+                    Assert.AreEqual(exception, e.GetType());
+                }
+            }
+        }
+
+        private static IEnumerable DeleteCharacterTestCases()
+        {
+            var repository = new MockPlayerBattleEntityRepository();
+            var commonEntityCommand = new PlayerCommonReconstructCommand(new CharacterID(), new PlayerName("TestCommonEntity"), new PlayerLevel(Experience.RequiredCumulativeExp(1)));
+            var commonEntity = PlayerFactory.Reconstruct(commonEntityCommand);
+            var command = new PlayerBattleReconstructCommand(commonEntity.ID, new CharacterBattleModule(new HealthPoint(commonEntity.Level), new LevelDependentParameters(commonEntity.Level, new Strength(1), new Agility(1)), new BattleEquipmentModule(null, null), new BattleStatusEffectModule(null)));
+
+            yield return new TestCaseData(repository, command, null).SetCategory("正常系");
+
+            // 存在しないIDのプレイヤーを削除しようとした場合
+            yield return new TestCaseData(repository, command, typeof(InvalidOperationException)).SetCategory("異常系");
+        }
+
+        [Test(Description = "DeleteCharacterのテスト")]
+        [TestCaseSource(nameof(DeleteCharacterTestCases))]
+        public async Task DeleteCharacterPasses(IPlayerBattleEntityRepository repository, PlayerBattleReconstructCommand command, Type? exception)
+        {
+            var usecase = new AdjustPlayerBattleEntityUsecase(repository);
+
+            if (exception is null)
+            {
+                var character = await usecase.ReconstructPlayer(command);
+
+                Assert.That(await repository.IsExist(character.ID), Is.True);
+
+                await usecase.DeletePlayer(character.ID);
+
+                Assert.That(await repository.IsExist(character.ID), Is.False);
+            }
+            else
+            {
+                try
+                {
+                    await usecase.DeletePlayer(new CharacterID());
+                }
+                catch (Exception e)
+                {
+                    Assert.AreEqual(exception, e.GetType());
+                }
+            }
+        }
+
+        private static IEnumerable GetCharacterTestCases()
+        {
+            var repository = new MockPlayerBattleEntityRepository();
+            var commonEntityCommand = new PlayerCommonReconstructCommand(new CharacterID(), new PlayerName("TestCommonEntity"), new PlayerLevel(Experience.RequiredCumulativeExp(1)));
+            var commonEntity = PlayerFactory.Reconstruct(commonEntityCommand);
+            var command = new PlayerBattleReconstructCommand(commonEntity.ID, new CharacterBattleModule(new HealthPoint(commonEntity.Level), new LevelDependentParameters(commonEntity.Level, new Strength(1), new Agility(1)), new BattleEquipmentModule(null, null), new BattleStatusEffectModule(null)));
+
+            yield return new TestCaseData(repository, command, null).SetCategory("正常系");
+
+            // 存在しないIDのプレイヤーを取得しようとした場合
+            yield return new TestCaseData(repository, command, typeof(InvalidOperationException)).SetCategory("異常系");
+        }
+
+        [Test(Description = "GetCharacterのテスト")]
+        [TestCaseSource(nameof(GetCharacterTestCases))]
+        public async Task GetCharacterPasses(IPlayerBattleEntityRepository repository, PlayerBattleReconstructCommand command, Type? exception)
+        {
+            var usecase = new AdjustPlayerBattleEntityUsecase(repository);
+
+            if (exception is null)
+            {
+                var character = await usecase.ReconstructPlayer(command);
+
+                var entity = await usecase.GetCharacter(character.ID);
+
+                if (entity is null)
+                    throw new NullReferenceException("RepositoryにBattleCharacterEntityが存在しません");
+
+                Assert.That(character.Equals(entity));
+            }
+            else
+            {
+                try
+                {
+                    await usecase.GetCharacter(new CharacterID());
+                }
+                catch (Exception e)
+                {
+                    Assert.AreEqual(exception, e.GetType());
+                }
+            }
+        }
+    }
+}
--- Assets/Plugins/BlackSmith.Domain/Usecase/Character/AdjustPlayerCommonEntityUsecase.cs
+++ Assets/Plugins/BlackSmith.Domain/Usecase/Character/AdjustPlayerCommonEntityUsecase.cs
@@ -4,16 +4,18 @@ using BlackSmith.Usecase.Interface;
 using Cysharp.Threading.Tasks;
 using System;
 
+#nullable enable
+
 namespace BlackSmith.Usecase.Character
 {
     /// <summary>
     /// CommonPlayerEntityの作成、削除を行うユースケース
     /// </summary>
-    public class AdjustPlayerCommonUsecase
+    public class AdjustPlayerCommonEntityUsecase
     {
         private readonly IPlayerCommonEntityRepository repository;
 
-        public AdjustPlayerCommonUsecase(IPlayerCommonEntityRepository playerRepository)
+        public AdjustPlayerCommonEntityUsecase(IPlayerCommonEntityRepository playerRepository)
         {
             repository = playerRepository;
         }
@@ -55,23 +57,24 @@ namespace BlackSmith.Usecase.Character
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        /// <exception cref="ArgumentException">指定したIdのキャラクターが存在しない場合</exception>
-        public async UniTask<PlayerCommonEntity> GetCharacter(CharacterID id)
+        public async UniTask<PlayerCommonEntity?> GetCharacter(CharacterID id)
         {
             if (!await repository.IsExist(id))
-                throw new ArgumentException($"指定したidのキャラクターは存在しません {id}");
+                return null;
 
-            var entity = (await repository.FindByID(id)) ?? throw new ArgumentException($"指定したidのキャラクターは存在しません {id}");
-
-            return entity;
+            return await repository.FindByID(id);
         }
 
         /// <summary>
         /// プレイヤーデータの削除を行う
         /// </summary>
         /// <param name="id">削除するプレイヤーのID</param>
+        /// <exception cref="InvalidOperationException">指定したidのキャラクターは存在しない場合</exception>
         public async UniTask DeletePlayer(CharacterID id)
         {
+            if (!await repository.IsExist(id))
+                throw new InvalidOperationException($"指定したidのキャラクターは存在しません {id}");
+
             await repository.Delete(id);
         }
     }
